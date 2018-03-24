4104616170b2c14a5d45c587b69c6ee4f71b4945
stevenshuang <stevenshuang521@gmail.com>
2018-03-24 18:56:51 +0800

    config file

---
+++ beautifulGirl/config/config.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+BASE_URL = 'http://www.mmjpg.com'
+
+
+ADDRESS = ('127.0.0.1', 8866)
+AUTHKEY = 'shuangr'.encode('utf-8')
+# email部分
+EMAIL_HOST_USER = 'zscoder1314@163.com'
+EMAIL_HOST_PASSWORD = 'Zs2521895377'
+EMAIL_PORT = 25
+EMAIL_HOST = 'smtp.163.com'
+
+PROXYS = [
+        {'http': '61.135.217.7:80',},
+        {'http': '122.114.31.177:808',},
+        {'http': '61.155.164.108:3128',},
+        {'https': '60.205.125.201:8888',},
+        {'https:': '114.115.140.25:3128',},
+        {'http': '61.155.164.108:3128',},
+        {'http': '61.155.164.111:3128',},
+        {'http': '123.7.38.31:9999',},
+        {'http': '139.129.166.68:3128',},
+        {'http': '114.113.126.86:80',},
+    ]
+
