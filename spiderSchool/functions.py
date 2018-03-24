b310d566aa47032fc67f065412981295e4cebc08
stevenshuang <stevenshuang521@gmail.com>
2018-03-24 19:07:53 +0800

    Add function file

---
+++ spiderSchool/functions.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import re
+import requests
+import pytesseract
+from datetime import datetime
+from bs4 import BeautifulSoup
+from model.mongoDb import DBManager
+from num_identify_code.identify_code import get_code_cookies
+from config.cs_config import *
+
+
+def login(WebUserNO: str, Password: str) -> None:
+
+    while True:
+        try:
+            captcha, cookies = get_code_cookies(BASE_URL+CAPTCHA_METHOD)
+            login_data = {
+                    'WebUserNO': WebUserNO,
+                    'Password': Password,
+                    'Agnomen': int(captcha)
+                }
+            login_request = requests.post(BASE_URL+LOGIN_METHOD, headers=HEADERS, 
+                    data=login_data, cookies=cookies)
+            res_data = login_request.content.decode('GBK')
+            if '正确的附加码' in res_data:
+                print ('验证码错误')
+            elif '错误的用户名' in res_data:
+                print ('用户名或者密码错误')
+            elif '欢迎您登录' in res_data:
+                print ('All right')
+                return cookies
+        except Exception as e:
+            print ('There are some wrong {}'.format(e))
+
+
+def get_term() -> int:
+
+    first_term_months = set([i for i in range(3, 9)])
+    second_term_months = set([i for i in range(1, 13)]) - first_term_months
+    date = datetime.now()
+    year, month = date.year, date.month
+    year -= 2010
+    if month in second_term_months:
+        return year*2+2
+    else:
+        return year*2-1
+
+
+def download_class_info(_class_id: str) -> dict:
+
+    cookies = login(TEACHER_ID, TEACHER_ID_PASSWORD)
+    post_data = {'ClassNO': _class_id}
+    res = requests.post(BASE_URL+CLASS_METHOD, headers=HEADERS, data=post_data, 
+            cookies=cookies)
+    sp_obj = BeautifulSoup(res.content, 'html.parser')
+    db_obj = DBManager()
+    for tr in sp_obj.find_all('table')[1].findAll('tr')[1:-1]:
+        row = tr.find_all('td')[1:4]
+        row_dict = dict(zip(CLASS_ID, [row_data.string for row_data in row]))
+        db_obj.insert_class_info(row_dict)
+        print (row_dict)
+
+
+def get_student_term_info(student_ID,  end_year_term_no):
+
+    flag = True
+    db = DBManager()
+    cookies = login(TEACHER_ID, TEACHER_ID_PASSWORD)
+    post_data = {
+            'YearTermNO': end_year_term_no,
+            'EndYearTermNO': end_year_term_no,
+            'ByStudentNO': int(student_ID)
+            }
+
+    student_name = db.get_student_name(student_ID)[0]['姓名']
+    try:
+        data_dcit = db.get_student_info_by_id(student_ID)[0]
+    except Exception as e:
+        falg = False
+        data_dcit = {'姓名': student_name, '学号': student_ID}
+
+    response = requests.post(BASE_URL+RESULT_METHOD, headers=HEADERS, data=post_data, 
+            cookies=cookies)
+    sp_obj = BeautifulSoup(response.content, 'html.parser')
+    all_tables = sp_obj.find_all('table')[4].find_all('tr')[1:]
+    for tr in all_tables:
+        tds = tr.find_all('td')
+        if tds[0].string == None:
+            break
+        else:
+            if tds[0].string not in data_dcit.keys():
+                data_dcit[tds[0].string] = {}
+            data_dcit[tds[0].string][tds[2].string] = {
+                    '开课模式': tds[3].srting,
+                    '总成绩': tds[-2].string,
+                    '绩点': tds[-1].string,
+                }
+    if not flag:
+        db.insert_sutdent_info(data_dcit)
+    else:
+        db.update_student_info(student_ID, data_dcit)
+    print (data_dcit)
+    return data_dcit
+
+
