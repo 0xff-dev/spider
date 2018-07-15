659449f104178832e41afe100195dd78af4bfd4b
stevenshuang <stevenshuang521@gmail.com>
2018-07-15 13:40:05 +0800

    去重

--- hdoj/src/models.py
+++ hdoj/src/models.py
@@ -14,6 +14,17 @@ class DBManager(object):
     def find_one(self, user):
         # 查询一个用户的全部信息
         return list(self.__collection.find_one({'user': user}))[0]
+    
+    def is_exists(self, user, p_id, date):
+        try:
+            result = list(self.__collection.find({'user': user},
+                                                {'date_ps.{}'.format(date)}))[0]
+            if p_id in result['date_ps'][date]:
+                return True
+            return False
+        except Exception as e:
+            # 数据中没有数据
+            return False
 
     def options_find(sefl, **kwargs):
         pass
