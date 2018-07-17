8a3f01430972b3e891fa9bee15fc6d52d4f005cb
stevenshuang <stevenshuang521@gmail.com>
2018-07-17 15:16:51 +0800

    添加统一爬取后的更新操作

--- hdoj/src/main.py
+++ hdoj/src/main.py
@@ -14,33 +14,37 @@ from models import DBManager
 BASE_URL = 'http://acm.hdu.edu.cn/'
 xpath = '/html/body/table/tbody/tr[4]/td/table/tbody/tr/td/p[3]'
 REQUEST_URL = 'http://acm.hdu.edu.cn/userstatus.php?user={user}'
+SOLVED_PS_XPATH = '/html/body/table/tbody/tr[4]/td/table/tbody/tr/td/table/tbody/tr[4]/td[2]'
 
 
 brower = webdriver.PhantomJS()
 db = DBManager()
 
 
+## 每道题只记录最近的ac
+
+
 def crawl_single_problem(user, p_id, url, _data: dict, db: DBManager):
     """
     return: date 2018-01-01 define
+    第综合抓取, 考虑存在同一道题存在在不同的时间内提交并通过
     """
     gevent.sleep(2)
     brower.get(url)
     lxml_html = etree.HTML(brower.page_source)
     xpath = '//*[@id="fixed_table"]/table/tbody/tr[2]/td[2]'
     date = lxml_html.xpath(xpath)[0].text.split(' ')[0]
-    print(date)
     # 获取提交的时间
     if date not in _data['date_ps'].keys():
         _data['date_ps'][date] = []
         _data['date_ps'][date].append(p_id)
     else:
-        if not db.is_exists(user, p_id, date):
+        if not db.is_exists_in_date(user, p_id, date):
             _data['date_ps'][date].append(p_id)
     print(_data)
 
 
-def crawl(user, db):
+def crawl(user, db, flag=False):
     """
     date: 2018-01-01
     """
@@ -50,12 +54,19 @@ def crawl(user, db):
     page_source = brower.page_source
     lxml_html = etree.HTML(page_source)
     tag_p = lxml_html.xpath(xpath)
+    all_ps = lxml_html.xpath(SOLVED_PS_XPATH)[0].text
     print(tag_p)
     print(tag_p[0].get('align'))
     tag_as = tag_p[0].findall('.//a')
     #user = User(user, " ")
-    _data = {"user": user, "nick_name": " ", "date_ps": {}}
     tasks = []
+    if not flag:
+        # 第一次写入数据库
+        _data = {"user": user, "nick_name": " ", "date_ps": {}, 'count': all_ps}
+    else:
+        _data = db.find_one(user)
+        if today not in _data['date_ps'].keys():
+            _data['date_ps'][today] = []
     for tag_a in tag_as:
         problem_id = tag_a.get('href').split('&')[1][4:]
         # 抛出id, +问题的url
@@ -63,7 +74,11 @@ def crawl(user, db):
                                   BASE_URL+tag_a.get('href'),
                                   _data, db)))
     gevent.joinall(tasks)
-    db.insert(_data)
+    if not flag:
+        db.insert(_data)
+    else:
+        db.update(user, _data)
+
 
 if __name__ == '__main__':
     # give me user list [(user, nick_name)]
