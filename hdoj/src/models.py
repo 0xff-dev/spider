577f335da9c488b022cdab445545d692fc5ce5ce
stevenshuang <stevenshuang521@gmail.com>
2018-07-17 15:16:30 +0800

    增加更新信息的操作

--- hdoj/src/models.py
+++ hdoj/src/models.py
@@ -13,9 +13,10 @@ class DBManager(object):
  
     def find_one(self, user):
         # 查询一个用户的全部信息
-        return list(self.__collection.find_one({'user': user}))[0]
+        return self.__collection.find_one({'user': user})
     
-    def is_exists(self, user, p_id, date):
+    def is_exists_in_date(self, user, p_id, date):
+        # 同一天做一道题，提交了若干次, 之记录一次
         try:
             result = list(self.__collection.find({'user': user},
                                                 {'date_ps.{}'.format(date)}))[0]
@@ -25,6 +26,17 @@ class DBManager(object):
         except Exception as e:
             # 数据中没有数据
             return False
+    
+    def is_exists_in_db(self, user, p_id):
+        # 查看这道题在数据库中是否存在
+        data = self.find_one(user)
+        for _, values in data['date_ps'].items():
+            if p_id in values:
+                return True
+        return False
 
-    def options_find(sefl, **kwargs):
-        pass
+    def update(self, user, _data: dict):
+        self.__collection.update_one({"user": user},
+                                     {'$set': {"date_ps": _data['date_ps'],
+                                               'count': _data['count']}})
+
